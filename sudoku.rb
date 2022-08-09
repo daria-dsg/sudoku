04a11c33c006364e2cd6fbc1e0bfe318d62acd6b
daria.dsg <daria.ui.ux@gmail.com>
2022-08-09 12:09:48 +0200

    Fix all bugs

--- sudoku.rb
+++ sudoku.rb
@@ -13,23 +13,23 @@ class SudokuGame
     @board = board
   end
 
-  def method_missing(method_name, *args)
-    if method_name =~ /val/
-      Integer(1)
-    else
-      string = args[0]
-      string.split(",").map! { |char| Integer(char) + 1 + rand(2) + " is the position"}
-    end
-  end
 
-  def get_pos
+def parse_pos(string)
+   string.split(",").map(&:to_i)
+end
+
+def parse_value(string)
+    Integer(string)
+end
+
+def get_pos
     pos = nil
     until pos && valid_pos?(pos)
       puts "Please enter a position on the board (e.g., '3,4')"
       print "> "
 
       begin
-        pos = parse_pos(gets)
+        pos = parse_pos(gets.chomp)
       rescue
         # TODO: Google how to print the error that happened inside of a rescue statement.
         puts "Invalid position entered (did you use a comma?)"
@@ -39,49 +39,49 @@ class SudokuGame
       end
     end
     pos
-  end
+end
 
-  def get_val
+def get_val
     val = nil
     until val && valid_val?(val)
       puts "Please enter a value between 1 and 9 (0 to clear the tile)"
       print "> "
-      val = parse_val(gets)
+      val = parse_value(gets)
     end
     val
-  end
+end
 
-  def play_turn
+def play_turn
     board.render
     pos = get_pos
     val = get_val
-    board[*pos] = val
-  end
+    board[pos] = val
+end
 
-  def run
+def run
     play_turn until solved?
     board.render
     puts "Congratulations, you win!"
-  end
+end
 
-  def solved?
-    self.solved?
-  end
+def solved?
+    board.solved?
+end
 
-  def valid_pos?(pos)
-    if pos.is_a?(:Array) &&
-      pos.length = 2 &&
-      pos.all? { |x| x.in?(0, board.size - 1) }
+def valid_pos?(pos)
+    if pos.is_a?(Array) &&
+      pos.length == 2 &&
+      pos.all? { |x| x.between?(0, board.size - 1) }
       return true
     else
       get_pos
     end
-  end
+end
 
-  def valid_val?(val)
+def valid_val?(val)
     val.is_a?(Integer) ||
       val.between?(0, 9)
-  end
+end
 
   private
   attr_reader :board
@@ -89,3 +89,4 @@ end
 
 
 game = SudokuGame.from_file("puzzles/sudoku1.txt")
+game.run
